8fb28b4640040fc7aba860fcdec0a561ccc876d0
koladmohamadhusain <husainkolad786@gmail.com>
2025-06-12 12:03:06 +0530

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({ username, email, password: hashedPassword });
+  await user.save();
+  res.json({ message: 'User registered' });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(400).json({ message: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+  res.json({ message: 'Login success', token });
+};
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/authRoutes'));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(process.env.PORT, () => console.log('Server started')))
+  .catch((err) => console.log(err));
